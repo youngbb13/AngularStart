2bfead0fe793bf985c326a68f999d21807de90c0
youngbb13 <153384410+youngbb13@users.noreply.github.com>
2026-03-10 16:19:52 +0100

    started

--- start/src/app/app.routes.ts
+++ start/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { Home } from './home/home';   
+
+export const routes: Routes = [
+  {
+    path: '',                    
+    component: Home
+  },
+  {
+    path: '**',                  
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
